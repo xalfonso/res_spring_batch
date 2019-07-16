b1ddc301896cd29327deb3a06cbbba1bc3880b8c
Eduardo Alfonso <eddie.alfonso@gmail.com>
2019-07-16 08:46:30 -0500

    ...

--- spring-batch-caos/spring-batch-services/src/main/java/eas/com/spring/batch/web/controller/DummyController.java
+++ spring-batch-caos/spring-batch-services/src/main/java/eas/com/spring/batch/web/controller/DummyController.java
@@ -1,6 +1,7 @@
 package eas.com.spring.batch.web.controller;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +25,7 @@ public class DummyController {
 
     @GetMapping("/run")
     public ResponseEntity runJob() throws Exception {
-        this.jobLauncher.run(job, new JobParameters());
+        JobExecution run = this.jobLauncher.run(job, new JobParameters());
         return ResponseEntity.ok().build();
     }
 }
